30d46cd866dfd264da9d8633241356c86ba83897
logixode <rohmad1230@gmail.com>
2019-12-28 20:20:29 +0700

    fix output information and floating logo

--- cpus.js
+++ cpus.js
@@ -2,5 +2,4 @@
 const os = require('os')
 const homeDir = os.cpus()[0].model
 
-const message = homeDir
-document.getElementById('cpus').innerHTML = `Processor : ${message}`
+document.getElementById('cpus').innerHTML = homeDir
